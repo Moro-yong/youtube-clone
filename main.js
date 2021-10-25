be710f172b408fa6af84031552390c550bd048af
Moro-yong <90921921+Moro-yong@users.noreply.github.com>
2021-10-25 18:56:57 +0900

    Fixing the file that I uploaded incorrectly.
    change files

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-const btn = document.querySelector('.info .btn');
-console.log(btn)
-const title = document.querySelector('.info .title');
-console.log(title)
 
-btn.addEventListener('click', () => {
-    btn.classList.toggle('clicked');
-    title.classList.toggle('clamp'); 
+//더보기 기능
+
+const infoMore = document.querySelector('.infoMore');
+console.log(infoMore);
+const infoTitle = document.querySelector('.infoTitle');
+console.log(infoTitle);
+
+//더보기 clamp
+infoMore.addEventListener('click', () => {
+    infoMore.classList.toggle('clicked');
+    infoTitle.classList.toggle('clamp');
+
+    //더보기 버튼 텍스트 변경
+    if(infoMore.innerText === '더보기') {
+        infoMore.innerText = '간략히';
+    } else infoMore.innerText = '더보기';
 });
 
-//위와 같음
-
-// btn.addEventListener('click', function() {
-//     btn.classList.toggle('clicked');
-//     title.classList.toggle('clamp'); 
-// });
