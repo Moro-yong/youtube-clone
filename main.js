ae8c0a3a93f0160e1bee45da64d4436f6ec10f87
Moro-yong <90921921+Moro-yong@users.noreply.github.com>
2021-10-27 14:10:01 +0900

    Add (dis)like button javsscript

--- main.js
+++ main.js
@@ -10,10 +10,30 @@ console.log(infoTitle);
 infoMore.addEventListener('click', () => {
     infoMore.classList.toggle('clicked');
     infoTitle.classList.toggle('clamp');
-
+    
     //더보기 버튼 텍스트 변경
     if(infoMore.innerText === '더보기') {
         infoMore.innerText = '간략히';
     } else infoMore.innerText = '더보기';
+});
+
+//좋아요 싫어요 버튼 색상 변경 * 숫자 up, down 설정해야함
+
+const like = document.querySelector('.fa-thumbs-up');
+const disLike = document.querySelector('.fa-thumbs-down');
+
+like.addEventListener('click', () => {
+    like.style.color = like.style.color === 'white' ? 'blue' : 'white';
+    if (like.style.color === 'blue') {
+        disLike.style.color = 'white';  // 색만 바뀔뿐 활성 비활성은 아닌거 같음...
+    }
+    
+});
+
+disLike.addEventListener('click', () => {
+    disLike.style.color = disLike.style.color === 'white' ? 'red' : 'white'
+    if (disLike.style.color === 'red') {
+        like.style.color = 'white';
+    }
 });
 
